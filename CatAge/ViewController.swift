59d8d7b898cb3d318ebbefa96b8f3c0e7e83a5c0
Alex Chaleroux <rayxx91@gmail.com>
2020-11-04 12:33:42 +0100

    Envoi Projet CatAge

--- CatAge/ViewController.swift
+++ CatAge/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var enterAgeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
+    
+    @IBAction func AnswerButton(_ sender: Any) {
+        
+        guard let number = Int(enterAgeTextField.text!)
+        else {
+            return 
+        }
+        
+        let catAge = Int(number) * 7
+        
+        enterAgeTextField.keyboardType = .numberPad
+        answerLabel.text = "Your cat is \(catAge) years !"
+    }
+    
+    
+    
 }
 
